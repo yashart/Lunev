66e31af8b596b5f61ab8bc5d69233d7f31a8fd45
yashart <yashart@mail.ru>
2016-04-15 16:45:56 +0300

    fault tolerance

--- SecondSem/2task/Mpi/main.cpp
+++ SecondSem/2task/Mpi/main.cpp
@@ -46,7 +46,7 @@ double calc_integral(Range* distance)
 void create_threads(int childrenQuantity, double startIntegral,
 					 double endIntegral, int procNumber)
 {
-	double start, end, result;
+	double start, end, result[2];
 	Range distance;
 	while(true)
 	{
@@ -56,12 +56,17 @@ void create_threads(int childrenQuantity, double startIntegral,
 				 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		if(start > end)
 			break;
+		result[1] = start;
 		distance = Range_ctor(start, end, step);
-		result = calc_integral(&distance);
-		MPI_Send(&result, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+		result[0] = calc_integral(&distance);
+		MPI_Send(&result, 2, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
 	}
 }
 
+void error_handler(MPI_Comm* comm, int* error, ...)
+{
+	printf("Error: %d", *error);
+}
 
 int main(int argc, char *argv[])
 {
@@ -78,7 +83,13 @@ int main(int argc, char *argv[])
 	double start = strtod(argv[1], &endptr);
 	double end = strtod(argv[2], &endptr);
 
+	int provided = 0;
 	MPI_Init(&argc, &argv);
+
+	//MPI_Errhandler newErrorHandler;
+	//MPI_Comm_create_errhandler(error_handler, &newErrorHandler);
+	//MPI_Comm_set_errhandler(MPI_COMM_WORLD, newErrorHandler);
+	
 	int childrenQuantity, procNumber;
 	MPI_Comm_rank(MPI_COMM_WORLD, &procNumber);
 	MPI_Comm_size(MPI_COMM_WORLD, &childrenQuantity);
@@ -89,42 +100,67 @@ int main(int argc, char *argv[])
 		globalResult = calc_integral(&distance);
 		MPI_Finalize();
 		endtime = MPI_Wtime();
-		printf("result = %lg, time = %lg\n", globalResult, endtime-starttime);
+		//printf("result = %lg, time = %lg\n", globalResult, endtime-starttime);
 		return 0;
 	}
 
 	if(procNumber == 0)
 	{
-		double localResult = 0;
+		double localResult[2];
+		int distanceQuantity = 30;
+		Range distancesQuery[distanceQuantity];
+		//Make distance Query
+		double part = (end - start)/distanceQuantity;
+		for(int i = 0; i < distanceQuantity; i++)
+		{
+			distancesQuery[i] = Range_ctor(start + i * part, 
+						start + (i + 1) * part, step);
+		}
 		//Send distance to child
 		int currentPoint = 0;
-		int distanceQuantity = 30;
-		double part = (end - start)/distanceQuantity;
 		Range distance;
 		for(int i = 1; i < childrenQuantity; i++)
 		{
-			distance = Range_ctor(start + currentPoint * part, 
-						start + (currentPoint + 1) * part, step);
-			MPI_Send(&distance.start, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
-			MPI_Send(&distance.end, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+			MPI_Send(&distancesQuery[currentPoint].start, 1,\
+					 MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+			MPI_Send(&distancesQuery[currentPoint].end, 1, \
+					 MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
 			currentPoint++;
 		}
 		//Get answer and send more!
 		MPI_Status status;
-		while(currentPoint < distanceQuantity)
+		int recieveMessagesNumbers = 0;
+		while(recieveMessagesNumbers < distanceQuantity - 1)
 		{
-			MPI_Recv(&localResult, 1, MPI_DOUBLE, MPI_ANY_SOURCE,
+			MPI_Recv(&localResult, 2, MPI_DOUBLE, MPI_ANY_SOURCE,
 					 0, MPI_COMM_WORLD, &status);
-			globalResult += localResult;
-			distance = Range_ctor(start + currentPoint * part, 
-						start + (currentPoint + 1) * part, step);
-			MPI_Send(&distance.start, 1, MPI_DOUBLE, status.MPI_SOURCE,
-					 0, MPI_COMM_WORLD);
-			MPI_Send(&distance.end, 1, MPI_DOUBLE, status.MPI_SOURCE,
-					 0, MPI_COMM_WORLD);
-			currentPoint++;
+			recieveMessagesNumbers ++;
+			globalResult += localResult[0];
+			//delete recieve message from query
+			for(int i = 0; i < distanceQuantity; i++)
+			{
+				if(distancesQuery[i].start == localResult[1])
+				{
+					distancesQuery[i].step = -1;
+					break;
+				}
+			}
+			while(true)
+			{
+				if(distancesQuery[currentPoint].step > 0)
+				{
+					MPI_Send(&distancesQuery[currentPoint].start, 1, MPI_DOUBLE, status.MPI_SOURCE,
+						 	0, MPI_COMM_WORLD);
+					MPI_Send(&distancesQuery[currentPoint].end, 1, MPI_DOUBLE, status.MPI_SOURCE,
+							 0, MPI_COMM_WORLD);
+					currentPoint = (currentPoint + 1) % distanceQuantity;
+					break;
+				}
+				currentPoint = (currentPoint + 1) % distanceQuantity;
+			}
 		}
 		//end all threads:
+		
 		double closeThread[2];
 		closeThread[0] = 0;
 		closeThread[1] = -1;
